0cd1277decdeff5960124f6921784f03ad98db52
Kevin Lee <kevin.code@kevinlee.io>
2025-12-06 20:45:06 +1100

    Refactor `MainApp` error handling and clean up `JokeRoutes`
    - Refactor `MainApp` to handle configuration loading errors using pattern matching instead of `EitherT`.
    - Remove unused `JokeRoutesErrorHandler`.

--- src/main/scala/cats_mtl_example/http/JokeRoutes.scala
+++ src/main/scala/cats_mtl_example/http/JokeRoutes.scala
@@ -7,7 +7,6 @@ import cats.syntax.all.*
 import cats_mtl_example.external.JokeClient
 import cats_mtl_example.external.types.HttpError
 import cats_mtl_example.http.types.ErrorMessage
-import io.circe.syntax.*
 import loggerf.core.Log
 import loggerf.syntax.all.*
 import org.http4s.*
@@ -38,44 +37,14 @@ object JokeRoutes {
           .rescue {
             case HttpError.ResponseBodyDecodingFailure(message, _) =>
               val errorMessage = s"Fetched joke but it couldn't be decoded: $message"
-              errorMessage.logS_(error) *> InternalServerError(ErrorMessage(errorMessage))
+              errorMessage.logS_(error) *>
+                InternalServerError(ErrorMessage(errorMessage))
+
             case err @ HttpError.FailedResponse(response) =>
               val errorMessage = s"Something went wrong: ${err.message}"
               errorMessage.logS_(error) *>
                 InternalServerError(ErrorMessage(errorMessage))
           }
     }
-// OR
-
-//    val jokeRoutesErrorHandler = JokeRoutesErrorHandler[F]
-//    HttpRoutes.of[F] {
-//      case GET -> Root / "joke" =>
-//        jokeRoutesErrorHandler
-//          .handle(
-//            for {
-//              joke <- jokeClient.getJoke
-//              resp <- Ok(joke)
-//            } yield resp
-//          )
-//    }
-
-  }
-
-  class JokeRoutesErrorHandler[F[*]: [G[*]] =>> MonadError[G, HttpError]]
-      extends RoutesErrorHandler[F, HttpError]
-      with Http4sDsl[F] {
-
-    private val handler: HttpError => F[Response[F]] = {
-      case HttpError.ResponseBodyDecodingFailure(message, _) =>
-        InternalServerError(s"Something went wrong: $message".asJson)
-      case err @ HttpError.FailedResponse(response) =>
-        InternalServerError(s"Something went wrong: ${err.message}".asJson)
-    }
-
-    val jokeRoutesErrorHandler: RoutesErrorHandler[F, HttpError] = RoutesErrorHandler(handler)
-
-    override def handle(routes: HttpRoutes[F]): HttpRoutes[F] =
-      jokeRoutesErrorHandler.handle(routes)
-
   }
 }
